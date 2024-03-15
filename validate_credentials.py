aeec06ee8620a22a1a2446cfcbe07a629c4faf59
yannickmondonguenextdecision <141943648+yannickmondonguenextdecision@users.noreply.github.com>
2024-03-15 10:42:24 +0100

    Add files via upload

---
+++ validate_credentials.py
@@ -0,0 +1,19 @@
+#openai
+import streamlit as st
+from openai import OpenAI
+
+client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
+
+completion = client.chat.completions.create(
+  model="gpt-3.5-turbo",
+  messages=[
+    {"role": "user", "content": "What is Streamlit?"}
+  ]
+)
+
+st.write(completion.choices[0].message.content)
+
+#snowflake
+conn = st.connection("snowflake")
+df = conn.query("select current_warehouse()")
+st.write(df)
